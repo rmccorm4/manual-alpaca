e9d07d155bd02f9feff9e09fedadf372ff2e9df0
Ryan McCormick <rmccorm4@binghamton.edu>
2019-03-03 07:53:43 +0000

    Remove TODO from test, trying to see if new tests work.

--- manual_alpaca/tests/test_auth.py
+++ manual_alpaca/tests/test_auth.py
@@ -8,7 +8,6 @@ import pytest
 from ..manual_alpaca import authenticate
 
 # TODO: Learn how to encrypt keys for TravisCI
-# Export real keys before testing
 #@pytest.mark.skip(reason="Not sure yet how to encrypt API keys in tests")
 @pytest.mark.parametrize("mode", ["live", "paper"])
 def test_auth_from_env_valid(mode):
